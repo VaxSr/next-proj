4993e7fdde9da59fbad29365963e1422102caaea
Valentin Sorohan <valentinsorohan@gmail.com>
2025-03-16 21:02:14 +0100

    finished code

---
+++ src/components/general/Navbar.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import {
+  getKindeServerSession,
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { buttonVariants } from "../ui/button";
+
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href={"/"}>
+          <h1 className="text-3xl font-semibold">
+            Blog<span className="text-blue-500">DelVaxone</span>
+          </h1>
+        </Link>
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            href="/"
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+          >
+            Home
+          </Link>
+          <Link
+            href="/dashboard"
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+      {user ? (
+        <div className="flex items-center gap-4">
+          <p>{user.given_name}</p>
+          <LogoutLink className={buttonVariants({ variant: "secondary" })}>
+            Logout
+          </LogoutLink>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <LoginLink className={buttonVariants()}>Login</LoginLink>
+          <RegisterLink className={buttonVariants({ variant: "outline" })}>
+            Sign up
+          </RegisterLink>
+        </div>
+      )}
+    </nav>
+  );
+}
